5354a2e47987f6674f4b27af82a6a4e463ab50cc
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-10-22 10:13:13 -0400

    Configuracion

---
+++ configuracion.php
@@ -0,0 +1,13 @@
+<?php
+$directory="sistemafacturacioncolegio/";		//Sub Directorio, Ej: "sistemacolegio/" 
+							//si su sistema e encuentre directamente en el dominio sin ningun subdirectorio dejelo vacio Ej: ""
+$Internet=0; //¿Donde se Encuentra su Sistema? 
+			// 0 en Sistema Local
+			// 1 en Internet
+			// 3 Solamente en LOCAL no Internet(Red de Area Local) 
+$UbicacionReporte="elalto";//miraflores
+
+                        
+$ta4="0aXzh++RAPaeH9XPUXzAaLjYAhOByL5jWvmip5hP0CSy2pP+8wyPegfbzLZu13exmtof0B3aEtM9Wid05YtJmQ==";
+$tp="D3W1N8E0E4B4B4";
+?>
