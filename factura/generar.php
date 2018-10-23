02483373153fca4235d6c74e5ab5b1e54df3c74e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-10-22 22:51:53 -0400

    Generar Factura

--- factura/generar.php
+++ factura/generar.php
@@ -1,5 +1,11 @@
 <?php
 function generarCodigoControl($NumeroAutorizacion,$NFactura,$Nit,$FechaCodigo,$TotalBsCodigo,$LlaveDosificacion){
+    /*echo "1:$NumeroAutorizacion<br>";
+    echo "2:$NFactura<br>";
+    echo "3:$Nit<br>";
+    echo "4:$FechaCodigo<br>";
+    echo "5:$TotalBsCodigo<br>";
+    echo "6:$LlaveDosificacion<br>";*/
     include_once("../../factura/facturacion1/codigocontrol.class.php");
     $CodigoControl=new CodigoControl($NumeroAutorizacion,$NFactura,$Nit,$FechaCodigo,$TotalBsCodigo,$LlaveDosificacion);
     $TxtCodigoDeControl=$CodigoControl->generar();
@@ -8,6 +14,7 @@ function generarCodigoControl($NumeroAutorizacion,$NFactura,$Nit,$FechaCodigo,$T
 
     $TxtCodigoDeControl2=CodigoControlV7::generar($NumeroAutorizacion, $NFactura, $Nit, $FechaCodigo, $TotalBsCodigo, $LlaveDosificacion);
     $datos=array("Codigo1"=>$TxtCodigoDeControl,"Codigo2"=>$TxtCodigoDeControl2);
+    //echo "7:$TxtCodigoDeControl2<br>";
     if($datos['Codigo1']==$datos['Codigo2']){
         return $datos;
     }else{
