60f3a2db3b2777603e2827bc70b8b51943abedee
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-10-30 17:21:16 -0400

    Generar

--- factura/generar.php
+++ factura/generar.php
@@ -6,11 +6,11 @@ function generarCodigoControl($NumeroAutorizacion,$NFactura,$Nit,$FechaCodigo,$T
     echo "4:$FechaCodigo<br>";
     echo "5:$TotalBsCodigo<br>";
     echo "6:$LlaveDosificacion<br>";*/
-    include_once("../../factura/facturacion1/codigocontrol.class.php");
+    include_once("facturacion1/codigocontrol.class.php");
     $CodigoControl=new CodigoControl($NumeroAutorizacion,$NFactura,$Nit,$FechaCodigo,$TotalBsCodigo,$LlaveDosificacion);
     $TxtCodigoDeControl=$CodigoControl->generar();
 
-    require_once("../../factura/facturacion2/CodigoControlV7.php");
+    require_once("facturacion2/CodigoControlV7.php");
 
     $TxtCodigoDeControl2=CodigoControlV7::generar($NumeroAutorizacion, $NFactura, $Nit, $FechaCodigo, $TotalBsCodigo, $LlaveDosificacion);
     $datos=array("Codigo1"=>$TxtCodigoDeControl,"Codigo2"=>$TxtCodigoDeControl2);
