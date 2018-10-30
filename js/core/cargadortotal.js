0748c54aaadf27a56ef7d19773ae94724cb9b1f0
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-10-30 17:49:57 -0400

    Cargador Total Js

--- js/core/cargadortotal.js
+++ js/core/cargadortotal.js
@@ -184,10 +184,10 @@ function inicio(){
 	$(document).on('click','#exportarexcel',function(e){
 		e.preventDefault();
 		var tabla=$(this).next().clone();
-		if(!(tabla.find("thead:eq(0)")).length){
+		/*if(!(tabla.find("thead:eq(0)")).length){
 			tabla=$(this).next().next().clone();
 		}
-		tabla.find("thead:eq(0)").remove();
+		tabla.find("thead:eq(0)").remove();*/
 		//alert(tabla.find("thead").html().remove());
 		var html='<table border="1">'+tabla.html()+'</table>';
 		//return false;
@@ -235,7 +235,47 @@ function inicio(){
 		e.preventDefault();
 		$('#noti').popover('hide');
 	}); 
+    
+    $(document).on("click","#exportarexcel1",function(e){
+        e.preventDefault();
+       fnExcelReport('tablaexportar');
+    });
 }
+
+function fnExcelReport(id)
+{
+    var tab_text="<table border='2px'><tr bgcolor='#87AFC6'>";
+    var textRange; var j=0;
+    tab = document.getElementById(id); // id of table
+
+    for(j = 0 ; j < tab.rows.length ; j++) 
+    {     
+        tab_text=tab_text+tab.rows[j].innerHTML+"</tr>";
+        //tab_text=tab_text+"</tr>";
+    }
+
+    tab_text=tab_text+"</table>";
+    tab_text= tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
+    tab_text= tab_text.replace(/<img[^>]*>/gi,""); // remove if u want images in your table
+    tab_text= tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE "); 
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer
+    {
+        txtArea1.document.open("txt/html","replace");
+        txtArea1.document.write(tab_text);
+        txtArea1.document.close();
+        txtArea1.focus(); 
+        sa=txtArea1.document.execCommand("SaveAs",true,"Say Thanks to Sumit.xls");
+    }  
+    else                 //other browser not tested on IE 11
+        sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));  
+
+    return (sa);
+}
+
 function cargandoG(destino){
 	$(destino).html('<img src="'+folder+'imagenes/cargador/cargador.gif"/>');
 }
