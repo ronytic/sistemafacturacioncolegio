47dfcf0ebc29c17c7fe9d1dc6dd92cd5013dc70a
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-10-30 18:51:52 -0400

    Listado Solo Curso

---
+++ listar/listadosolocurso.php
@@ -0,0 +1,30 @@
+<?php include_once($folder."login/check.php");?>
+<?php
+include_once(RAIZ."class/curso.php");
+include_once(RAIZ."class/alumno.php");
+if(isset($_GET['CodAlumno'])){
+	$CodAlumno=$_GET['CodAlumno'];
+	$alumno=new alumno;
+	$al=array_shift($alumno->mostrarTodoDatos($CodAlumno));
+}
+if(isset($_GET['CodCurso'])){
+	$CodCurso=$_GET['CodCurso'];
+}
+if(empty($CodCurso)){
+	$CodCurso=$al['CodCurso'];
+}
+$curso=new curso;
+?>
+            <select class="form-control" id="selectcurso" name="CodCurso" data-placeholder="Seleccione un Curso" >
+                <option value="">Seleccionar</option>
+            <?php
+			$i=0;
+            foreach($curso->mostrar() as $cu){$i++;
+                ?><option value="<?php echo $cu['CodCurso'];?>" <?php //echo $i==1 || $CodCurso==$cu['CodCurso']?'selected="selected"':'';?> rel="<?php echo $cu['caArea']?>"><?php echo eliminarEspaciosDobles($cu['Nombre']);?></option><?php
+            }
+            ?>
+            </select>
+
+
+
+        
