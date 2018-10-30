c5cc779fd0defd1c6b4c14c34648a734f8ddd313
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-10-30 17:21:24 -0400

    Probar Factura

--- factura/probar.php
+++ factura/probar.php
@@ -1,8 +1,18 @@
 <?php
 $Nit="1665979";
-NFactura:"876814"
-3:"1665979"
-4:"20080519"
-5:"35959"
-6:zZ7Z]xssKqkEf_6K9uH(EcV+%x+u[Cca9T%+_$kiLjT8(zr3T9b5Fx2xG-D+_EBS
+$NFactura="876814";
+$NAutorizacion="7904006306693";
+$FechaFactura="20080519";
+$Monto=35958.6;
+$Monto=round(str_replace(',', '.', $Monto), 0);
+    //k##MCZE4P7M{2XSKpF\[*)B{et@6U{-62A8ATN2NHAQCTs{BJ6g@7p3KI+_Q=9S3
+require_once("../class/config.php");
+$config=new config;
+$LlaveDosificacion=($config->mostrarConfig("LlaveDosificacion",1));
+echo $LlaveDosificacion;
+include_once("generar.php");
+$d= generarCodigoControl($NAutorizacion,$NFactura,$Nit,$FechaFactura,$Monto,$LlaveDosificacion);
+echo "<pre>";
+print_r($d);
+echo "</pre>";
 ?>
