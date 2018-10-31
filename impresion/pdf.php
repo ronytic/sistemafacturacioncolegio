21e880ca3fad5206196d17ad2b60e2e7ec983942
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-10-31 15:53:17 -0400

    PDF

--- impresion/pdf.php
+++ impresion/pdf.php
@@ -1,32 +1,43 @@
 <?php
-include_once("fpdf_protection.php");
-	if(!defined("Config")){
-		include_once("../../class/config.php");
-	}
-	if(!isset($config)){
+include_once("fpdf/fpdf.php");
+$logo="logo.jpg";
+
+if(!defined("Config")){
+	include_once("../../class/config.php");
+}
+
+if(!isset($config)){
 		$config=new config;
 	}
-	$cnf=$config->mostrarConfig("Titulo");
-	$title=$cnf['Valor'];
-	$cnf=$config->mostrarConfig("Gestion");
-	$gestion=$cnf['Valor'];
-	$cnf=$config->mostrarConfig("Lema");
-	$lema=$cnf['Valor'];
-	$cnf=$config->mostrarConfig("Logo");
-	$logo=$cnf['Valor'];
-	include_once("../../funciones/usuarios.php");
-	$Nivel=$_SESSION['Nivel'];
-	$CodigoUsuario=$_SESSION['CodUsuarioLog'];
-	$DatosGenerador=DatosUsuario($Nivel,$CodigoUsuario);
-	class PPDF extends FPDF_Protection{
+$title=$config->mostrarConfig("Titulo",1);
+$gestion=date("Y");
+$lema=$config->mostrarConfig("Lema",1);
+$logo=$config->mostrarConfig("Logo",1);
+
+
+include_once("../../funciones/usuarios.php");
+$Nivel=$_SESSION['Nivel'];
+
+$CodigoUsuario=$_SESSION['CodUsuarioLog'];
+
+
+include_once("../../class/usuario.php");
+$us100=new usuario;
+$ul=$us100->mostrarDatos($CodigoUsuario);
+$ul=array_shift($ul);
+$DatosGenerador=array("Paterno"=>$ul['Paterno'],
+		"Materno"=>$ul['Materno'],
+		"Nombres"=>$ul['Nombres']);
+
+	class PPDF extends FPDF{
 		var $ancho=176;
         var $altocelda=5;
 		function Header(){
 			global $idioma,$FechaReporte;
-			$this->SetTitle(utf8_decode("Sistema de Administración de Inventario, Ventas y Facturación"),true);
-			$this->SetAuthor(utf8_decode("Sistema de Administración de Inventario, Ventas y Facturación Desarrollado por Ronald Nina Layme. Cel: 73230568 - www.facebook.com/ronaldnina"),true);
-			$this->SetSubject(utf8_decode("Sistema de Administración de Inventario, Ventas y FacturaciónDesarrollado por Ronald Nina Layme. Cel: 73230568 - www.facebook.com/ronaldnina"),true);
-			$this->SetCreator(utf8_decode("Sistema de Administración de Inventario, Ventas y Facturación Desarrollado por Ronald Nina Layme. Cel: 73230568 - www.facebook.com/ronaldnina"),true);
+			$this->SetTitle(utf8_decode("Sistema de Administración de Facturación de Colegios"),true);
+			$this->SetAuthor(utf8_decode("SSistema de Administración de Facturación de Colegios Desarrollado por Ronald Nina Layme. Cel: 73230568 - www.facebook.com/ronaldnina"),true);
+			$this->SetSubject(utf8_decode("Sistema de Administración de Facturación de Colegios Desarrollado por Ronald Nina Layme. Cel: 73230568 - www.facebook.com/ronaldnina"),true);
+			$this->SetCreator(utf8_decode("Sistema de Administración de Facturación de Colegios Desarrollado por Ronald Nina Layme. Cel: 73230568 - www.facebook.com/ronaldnina"),true);
 			//$this->SetProtection(array('print'));
 			if($this->CurOrientation=="P"){$this->ancho=$this->w-40;}else{$this->ancho=$this->w-33.4;}
 			$this->SetLeftMargin(18);
@@ -35,20 +46,20 @@ include_once("fpdf_protection.php");
 			$fecha=capitalizar(strftime("%A, %d "))." de ".capitalizar(strftime(" %B "))." de ".strftime(" %Y");
 			
 			$this->Image("../../imagenes/logo/".$logo,10,10,20,20);
-			$this->Fuente("B",$tam);
+			$this->Fuente("B",10);
 			$this->SetXY(34,12);
 			$this->Cell(70,4,utf8_decode($title),0,0,"L");
 			$this->Fuente("B",8);
 			$this->SetXY(34,16);
 			$this->Cell(70,4,utf8_decode($gestion),0,0,"L");
 			$this->ln(10);	
-			$this->Fuente("B",18);
+			$this->Fuente("B",14);
 			$this->Cell($this->ancho,4,utf8_decode($titulo),0,5,"C");
 			$this->ln(5);
-            if(!isset($FechaReporte)){
+            /*if(!isset($FechaReporte)){
 			    $this->CuadroCabecera(32,"Fecha del Reporte: ",50,utf8_encode($fecha));
                 $this->ln(5);
-            }
+            }*/
 			
 			if(in_array("Cabecera",get_class_methods($this))){
 				$this->Cabecera();	
@@ -61,13 +72,18 @@ include_once("fpdf_protection.php");
 		function Pagina(){
 			global $idioma;
 			$this->AliasNbPages();
-			$this->CuadroCabecera(15,$idioma['Pagina'].":",20,$this->PageNo()." ".$idioma['De']." {nb}");
+			$this->CuadroCabecera(15,"Página:",20,$this->PageNo()." de {nb}");
 		}
         function AltoCelda($a){
             $this->altocelda=$a;    
         }
-		function Fuente($tipo="B",$tam=10){
-			$this->SetFillColor(234,234,234);
+		function Fuente($tipo="B",$tam=10,$TipoRelleno=2){
+            if($TipoRelleno==1){
+                $this->SetFillColor(204,204,204);
+            }
+            if($TipoRelleno==2){
+                $this->SetFillColor(184,184,184);
+            }
 			$this->SetFont("Arial",$tipo,$tam);	
 		}
 		function CuadroCabecera($txt1Ancho,$txt1,$txt2Ancho,$txt2){
@@ -157,12 +173,12 @@ include_once("fpdf_protection.php");
 			
 			
 			if($this->CurOrientation=="P" ||$this->OrientacionObligada=="L"){
-				$this->Cell((round(($this->ancho-50)/2)+0),3,utf8_decode("Sistema de Administración de Inventario, Ventas y Facturación"),$BordePie,0,"R");
+				$this->Cell((round(($this->ancho-50)/2)+0),3,utf8_decode("Sistema de Administración de Facturación de Colegios"),$BordePie,0,"R");
 				$this->ln();
 				$this->Cell((round(($this->ancho-50)/2)+50),3,"Usuario: ".$DatosUsuario,$BordePie,0,"L");
 				$this->Cell((round(($this->ancho-50)/2)+00),3,"Desarrollado por Ronald Nina",$BordePie,0,"R");
 			}else{
-				$this->Cell((round(($this->ancho-50)/2)-5),3,utf8_decode("Sistema de Administración de Inventario, Ventas y Facturación - Desarrollado por Ronald Nina"),$BordePie,0,"R");	
+				$this->Cell((round(($this->ancho-50)/2)-5),3,utf8_decode("Sistema de Administración de Facturación de Colegios - Desarrollado por Ronald Nina"),$BordePie,0,"R");	
 			}
 			
 			//$this->Cell(60,4,utf8_decode($idioma['ReporteGenerado']).": ".date('d-m-Y H:i:s'),0,0,"R");
