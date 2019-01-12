610444839601562eba6b3776c8f2a8f225cd5c5c
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-01-12 11:52:29 -0400

    Factura Actualizar

--- factura/listado/actualizar.php
+++ factura/listado/actualizar.php
@@ -5,4 +5,27 @@ include_once("../../class/factura.php");
 $factura=new factura;
 $valores=array("Estado"=>"'$estado'");
 $factura->actualizarRegistro($valores,"codfactura=".$codfactura);
+
+include_once("../../class/facturadetalle.php");
+$facturadetalle=new facturadetalle;
+
+include_once("../../class/cuota.php");
+$cuota=new cuota;
+
+if($estado=="Anulado"){
+	$facturad=$facturadetalle->mostrarFacturaDetalleCod($codfactura);
+	foreach($facturad as $fd){
+		$CodigosCuota=$fd['CodCuota'];
+		$cuota->actualizarCuota(array("Cancelado"=>0,"Observaciones"=>"'Anulado'"),"CodCuota IN($CodigosCuota)");		
+	}
+}else{
+	$facturad=$facturadetalle->mostrarFacturaDetalleCod($codfactura);
+	foreach($facturad as $fd){
+		$CodigosCuota=$fd['CodCuota'];
+		$cuota->actualizarCuota(array("Cancelado"=>1,"Observaciones"=>"'Facturado-Rehabilitado'"),"CodCuota IN($CodigosCuota)");		
+	}
+}
+
+
+
 ?>
