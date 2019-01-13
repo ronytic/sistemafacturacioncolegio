5850de807d6867555080b6da8dfc610afaa8d89b
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-01-12 21:54:30 -0400

    Corregido Revisar

--- rastreo/revisar.php
+++ rastreo/revisar.php
@@ -6,11 +6,11 @@ $Session="";
 $Ip=$_SERVER['REMOTE_ADDR'];
 @$Referencia=$_SERVER['HTTP_REFERER'];
 foreach($_GET as $k =>$v)
-	$Get.="$k=$v|";
+	@$Get.="$k=$v|";
 foreach($_POST as $k =>$v)
-	$Post.="$k=$v|";
+	@$Post.="$k=$v|";
 foreach($_SESSION as $k =>$v)
-	$Session.="$k=$v|";
+	@$Session.="$k=$v|";
 //$Ip=ip2long($Ip);
 include_once(RAIZ."class/lograstreo.php");
 $lograstreo=new lograstreo;
