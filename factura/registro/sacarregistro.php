d39be50da80839d562136b682306112e36fa481d
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-01-12 21:55:28 -0400

    Sacar Registro Factura

--- factura/registro/sacarregistro.php
+++ factura/registro/sacarregistro.php
@@ -9,24 +9,24 @@ if(!empty($_POST['CodAlumno'])){
 	$cuota=new cuota;
 	$CodAlumno=$_POST['CodAlumno'];
 	$Registro=$_POST['Registro'];
-    
+
 	$al=$alumno->mostrarTodoDatos($CodAlumno);
 	$al=array_shift($al);
-    
+
 	$cuo=$cuota->mostrarCuotasNoCanceladas($CodAlumno);
 	//$cuo=array_shift($cuo);
-    
+
     $cur=$curso->mostrarCurso($al['CodCurso']);
 	$cur=array_shift($cur);
-    
+
 	if(!count($cuo)){
-		$cuo['Numero']="SinDeuda";	
+		$cuo['Numero']="SinDeuda";
 	}
     $datos="";
     $ij=0;
     foreach($cuo as $c){
         //$datos.='<input value="'.$c['CodCuota']."\">C".$c['Numero'].'</option>';
-        $datos.='<label class="opcion" rel="'.$Registro.'"><input type="checkbox" name="a['.$Registro.'][Cuota]['.$c['CodCuota'].']" value="'.$c[Numero].'" class="opcionCuota" rel="'.$Registro.'"><div>'.$c[Numero].'</div></label>';
+        $datos.='<label class="opcion" rel="'.$Registro.'"><input type="checkbox" name="a['.$Registro.'][Cuota]['.$c['CodCuota'].']" value="'.$c['Numero'].'" class="opcionCuota" rel="'.$Registro.'"><div>'.$c['Numero'].'</div></label>';
         if($ij==1){
             $datos.="<br>";
             $ij=0;
@@ -34,7 +34,7 @@ if(!empty($_POST['CodAlumno'])){
             $ij++;
         }
     }
-    
+
 	$nombres=capitalizar($al['Paterno'])." ".capitalizar($al['Materno'])." ".capitalizar($al['Nombres'])." ";
 	$valores=array("Alumno"=>$nombres,
 					"Cuota"=>$datos,
@@ -42,7 +42,7 @@ if(!empty($_POST['CodAlumno'])){
 					"Registro"=>$Registro,
 					"CodAlumno"=>$CodAlumno
 					);
-	
+
 	echo json_encode($valores);
 }
 ?>
