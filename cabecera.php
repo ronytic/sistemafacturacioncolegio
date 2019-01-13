5c293a00cb307dfe46722f1fd7497bc2ad7099c7
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-01-13 12:49:48 -0400

    Cabera

--- cabecera.php
+++ cabecera.php
@@ -253,10 +253,11 @@
         </div>
         <div class="row">
         <!------------>
-        <div class="col-lg-12">
+        <div class="col-lg-<?php echo isset($columna1)?$columna1:'12';?>">
             <div class="hpanel">
                 <div class="panel-heading">
                     <div class="panel-tools">
+
                         <!--<a class="showhide"><i class="fa fa-chevron-up"></i></a>-->
                         <!--<a class="closebox"><i class="fa fa-times"></i></a>-->
                     </div>
