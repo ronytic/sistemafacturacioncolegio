80dda5d1371e54fd62894b1bb0312ad6d45e46ee
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2019-01-13 18:55:22 -0400

    Modificar Rude

---
+++ rude/modificar/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once("../../login/check.php");
+$titulo="Listado de RUDE de Alumnos";
+$folder="../../";
+
+$columna1=4;
+$subtitulo3="Datos del RUDE";
+$archivodestinoalumno="rude/modificar/registro.php";
+require_once("../../listar/listarcursoalumno.php");
+?>
---
+++ rude/registro/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once("../../login/check.php");
+$titulo="Listado de RUDE de Alumnos";
+$folder="../../";
+
+$columna1=4;
+$subtitulo3="Datos Estadísticos";
+$archivodestinoalumno="alumno/registro/dato.php";
+require_once("../../listar/listarcursoalumno.php");
+?>
