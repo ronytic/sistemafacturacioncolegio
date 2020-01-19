308a2758d69f766d4d0b838d62507533b3f02d85
Ronald Nina <ronald.nina.layme@gmail.com>
2020-01-19 13:30:33 -0400

    LF

--- js/listar.js
+++ js/listar.js
@@ -1,31 +1,31 @@
-$(document).ready(function() {
-  $(".radiolistadocurso").click(function(event) {
-    var CodCurso=$(this).val();
-    var CodAlumno=$(this).attr('rel');
-    $.post(folder+"listar/alumnos.php",{'CodCurso':CodCurso,"listaalumno":1,'CodAlumno':CodAlumno},function(data){
+$(document).ready(function () {
+  $(".radiolistadocurso").click(function (event) {
+    var CodCurso = $(this).val();
+    var CodAlumno = $(this).attr('rel');
+    $.post(folder + "listar/alumnos.php", { 'CodCurso': CodCurso, "listaalumno": 1, 'CodAlumno': CodAlumno }, function (data) {
       $("#respuestacurso").html(data);
 
-      if($(".radiolistadoalumno:checked").val()==undefined){
+      if ($(".radiolistadoalumno:checked").val() == undefined) {
         $(".radiolistadoalumno:eq(0)").click();
-      }else{
+      } else {
         $(".radiolistadoalumno:checked").click();
       }
 
     });
   });
-if($(".radiolistadocurso:checked").val()==undefined){
-  $(".radiolistadocurso:eq(0)").click();
-}else{
-  $(".radiolistadocurso:checked").click();
-}
+  if ($(".radiolistadocurso:checked").val() == undefined) {
+    $(".radiolistadocurso:eq(0)").click();
+  } else {
+    $(".radiolistadocurso:checked").click();
+  }
 
 
-  $(document).on("click",".radiolistadoalumno",function(event) {
+  $(document).on("click", ".radiolistadoalumno", function (event) {
 
-    var CodAlumno=$(this).val();
+    var CodAlumno = $(this).val();
     //alert(CodAlumno);
-    var CodCurso=$(".radiolistadocurso:checked").val();
-    $.post(folder+archivodestinoalumno,{'CodCurso':CodCurso,"CodAlumno":CodAlumno},function(data){
+    var CodCurso = $(".radiolistadocurso:checked").val();
+    $.post(folder + archivodestinoalumno, { 'CodCurso': CodCurso, "CodAlumno": CodAlumno }, function (data) {
       $("#respuestaalumno").html(data);
     });
   });
