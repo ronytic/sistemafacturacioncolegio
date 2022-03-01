12854f19b13366f49bb983e575542811c52d88ba
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2022-03-01 13:29:42 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,18 +11,25 @@ $titulo2=" ";
                 <div class="col-lg-10 col-lg-offset-1">
                     <h3 class="text-center">Bienvenido al Sistema de Facturación</h3>
                     <hr>
-                    <div class="col-lg-5">
-                    <a class="btn btn-info form-control" href="alumno/registro/">Registro de Alumnos</a>
-                    <br><br>
-                    <a class="btn btn-info form-control" href="curso/registro/">Registro de Cursos</a>
-                    <br><br>
-                    <a class="btn btn-primary form-control" href="factura/registro/">Registro de Facturación</a>
-                    <br><br>
-                    <a class="btn btn-primary form-control" href="configuracion/general/">Configuración General del Sistema</a>
-                    <br><br>
-                    <a class="btn btn-danger form-control" href="factura/activar/">Verificación de Código de Control</a>
-                    <br><br>
-                    <a class="btn btn-danger form-control">Reporte de Libro de Ventas</a>
+                    <div class="col-lg-4">
+                        <a class="btn btn-info form-control" href="alumno/registro/">Registro de Alumnos</a>
+                        <br><br>
+                        <a class="btn btn-info form-control" href="curso/registro/">Registro de Cursos</a>
+                        <br><br>
+                    
+                    </div>
+                    <div class="col-lg-4">
+                        <a class="btn btn-primary form-control" href="factura/registro/">Registro de Facturación</a>
+                        <br><br>
+                        <a class="btn btn-primary form-control" href="configuracion/general/">Configuración General del Sistema</a>
+                        <br><br>
+                        
+                    </div>
+                    <div class="col-lg-4">
+                        
+                        <a class="btn btn-danger form-control" href="factura/activar/">Verificación de Código de Control</a>
+                        <br><br>
+                        <a class="btn btn-danger form-control" href="factura/reportefacturas/">Reporte de Libro de Ventas</a>
                     </div>
                 </div>
 <?php include_once("pie.php");?>
